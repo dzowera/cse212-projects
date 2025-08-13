cb3c8fcacdd688f8e560b0fd029b098f8c6b26ec
dzowera <idzowera@byupathway.edu>
2025-08-13 10:09:55 +0200

    week 06

--- week06/code/Node.cs
+++ week06/code/Node.cs
@@ -21,7 +21,7 @@ public class Node
             else
                 Left.Insert(value);
         }
-        else
+        else if (value > Data)
         {
             // Insert to the right
             if (Right is null)
@@ -34,12 +34,33 @@ public class Node
     public bool Contains(int value)
     {
         // TODO Start Problem 2
-        return false;
+        // return false;
+        if (value == Data)
+        {
+            // found it
+            return true;
+        }
+
+        else if (value < Data)
+        {
+            // search left if left exists
+            return Left != null && Left.Contains(value);
+        }
+
+        else
+        {
+            return Right != null && Right.Contains(value);
+        }
     }
 
     public int GetHeight()
     {
         // TODO Start Problem 4
-        return 0; // Replace this line with the correct return statement(s)
+        // return 0; // Replace this line with the correct return statement(s)
+
+        int leftHeight = Left != null ? Left.GetHeight() : 0;
+        int rightHeight = Right != null ? Right.GetHeight() : 0;
+
+        return 1 + Math.Max(leftHeight, rightHeight);
     }
 }
